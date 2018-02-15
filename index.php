1413d55024bae1a0a3bbf39e2a5be16562035968
avadakkeddavra <avadakkeddavra@gmail.com>
2018-02-15 16:32:31 +0200

    add folder files to git ignore

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php 
-	ini_set('error_reporting', E_ALL);
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-
 	require_once __DIR__.'/functions.php';
 
 
@@ -24,6 +20,7 @@
 		
 	}else{
 		getView();
+
 	}
 	
  ?>
